db72c982c9553340003a6580662d1305dffab64a
ranga sumanth <rangasumanthgoud@gmail.com>
2025-01-30 11:31:48 -0800

    updated to json response

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,4 +52,5 @@ if ingredients_list:
 
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
+
